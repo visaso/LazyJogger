51ccd84b9b415f9ed35f137c4fdc9b3b51345bd2
visaso <32449867+visaso@users.noreply.github.com>
2019-10-01 14:16:42 +0300

    Test commit

--- app/src/main/java/com/example/lazyjogger/MainActivity.kt
+++ app/src/main/java/com/example/lazyjogger/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         tabs.setupWithViewPager(viewPager)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Test with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
     }
