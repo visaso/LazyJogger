00453e493043f3677fce1fc15bdfb03e3916a2e1
visaso <32449867+visaso@users.noreply.github.com>
2019-10-06 19:36:12 +0300

    Heatmap working, yet to implement collaboration with heartbeat

--- app/src/main/java/com/example/lazyjogger/ui/main/RunMenuFragment.kt
+++ app/src/main/java/com/example/lazyjogger/ui/main/RunMenuFragment.kt
@@ -2,6 +2,7 @@ package com.example.lazyjogger.ui.main
 
 
 import android.graphics.Color
+import android.graphics.ColorSpace
 import android.location.Location
 import android.os.Bundle
 import android.os.Looper
@@ -12,12 +13,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.core.graphics.blue
+import androidx.core.graphics.green
+import androidx.core.graphics.red
 import androidx.fragment.app.Fragment
 import com.example.lazyjogger.R
 import com.example.lazyjogger.database.User
 import com.example.lazyjogger.database.UserDB
 import com.google.android.gms.location.*
 import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.gray
 import org.jetbrains.anko.uiThread
 import org.osmdroid.bonuspack.routing.OSRMRoadManager
 import org.osmdroid.bonuspack.routing.Road
@@ -31,6 +36,8 @@ import org.osmdroid.views.overlay.OverlayItem
 import org.osmdroid.views.overlay.Polyline
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
+import kotlin.math.abs
+import kotlin.math.roundToInt
 import kotlin.random.Random
 
 /**
@@ -43,6 +50,8 @@ class RunMenuFragment : Fragment(), LocationListener {
 
     private lateinit var locationCallback: LocationCallback
     private lateinit var locationRequest: LocationRequest
+
+    private lateinit var previousLocation: GeoPoint
 
     override fun onLocationChanged(p0: Location?) {
         Log.d("New latitude", "latitude: ${p0?.latitude}")
@@ -60,13 +69,15 @@ class RunMenuFragment : Fragment(), LocationListener {
             PreferenceManager.getDefaultSharedPreferences(context))
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(context!!)
         fusedLocationClient.lastLocation.addOnCompleteListener { task ->
-            if (task.isSuccessful && task.result != null) {
-                Log.d("LOCATION", "latitude: ${task.result!!.latitude}")
-                Log.d("LOCATION", "longitude: ${task.result!!.longitude}")
-                map.controller.setCenter(GeoPoint(task.result!!.latitude, task.result!!.longitude))
-                addPoint(GeoPoint(task.result!!.latitude, task.result!!.longitude))
+            if (task.isSuccessful && task.result != null && !this::previousLocation.isInitialized) {
+                previousLocation = GeoPoint(task.result!!.latitude, task.result!!.longitude)
+//                Log.d("LOCATION", "latitude: ${task.result!!.latitude}")
+//                Log.d("LOCATION", "longitude: ${task.result!!.longitude}")
+//                map.controller.setCenter(GeoPoint(task.result!!.latitude, task.result!!.longitude))
+//                addPoint(GeoPoint(task.result!!.latitude, task.result!!.longitude))
             }
         }
+
 
         locationRequest = LocationRequest()
         locationRequest.interval = 1500
@@ -75,8 +86,10 @@ class RunMenuFragment : Fragment(), LocationListener {
             override fun onLocationResult(locationResult: LocationResult?) {
                 locationResult ?: return
                 for (location in locationResult.locations){
+                    val loc = GeoPoint(location.latitude, location.longitude)
                     Toast.makeText(context, "${location.latitude}", Toast.LENGTH_SHORT).show()
-                    addPoint(GeoPoint(location.latitude, location.longitude))
+                    addPoint(loc)
+                    previousLocation = loc
                 }
             }
         }
@@ -125,27 +138,54 @@ class RunMenuFragment : Fragment(), LocationListener {
         mLocationOverlay.enableMyLocation()
         map.overlays.add(mLocationOverlay)
         polyline = Polyline(map)
-//        var startPoint = GeoPoint(60.271585, 24.826612)
-//        var endPoint = GeoPoint(60.297056, 24.917141)
-//        polyline.addPoint(startPoint)
-//        polyline.addPoint(endPoint)
         map.overlays.add(polyline)
 
     }
 
     private fun addPoint(geoPoint: GeoPoint) {
         val r = Random
-        val lol = r.nextInt(5)
+        val lol = r.nextInt(10)
+        val newLine = Polyline()
+
         when (lol) {
-            1 -> polyline.color = Color.RED
-            2 -> polyline.color = Color.GREEN
-            else -> polyline.color = Color.BLUE
+            1 -> newLine.color = Color.parseColor(calculateColor(0.1.toFloat()))
+            2 -> newLine.color = Color.parseColor(calculateColor(0.2.toFloat()))
+            3 -> newLine.color = Color.parseColor(calculateColor(0.3.toFloat()))
+            4 -> newLine.color = Color.parseColor(calculateColor(0.4.toFloat()))
+            5 -> newLine.color = Color.parseColor(calculateColor(0.5.toFloat()))
+            6 -> newLine.color = Color.parseColor(calculateColor(0.6.toFloat()))
+            7 -> newLine.color = Color.parseColor(calculateColor(0.6.toFloat()))
+            8 -> newLine.color = Color.parseColor(calculateColor(0.7.toFloat()))
+            9 -> newLine.color = Color.parseColor(calculateColor(0.8.toFloat()))
+            else -> newLine.color = Color.parseColor(calculateColor(0.8.toFloat()))
         }
-        //polyline.color = Color.RED
-        polyline.addPoint(geoPoint)
+
+        //polyline.color = Color.RED'
+
+
+        newLine.addPoint(previousLocation)
+        newLine.addPoint(geoPoint)
+        map.controller.setCenter(geoPoint)
         //map.overlays.add(polyline)
-        map.overlays.add(polyline)
+        map.overlays.add(newLine)
         map.invalidate()
+    }
+
+    private fun calculateColor(strength: Float): String {
+        val greenColor = Color.parseColor("#66d48f")
+        val redColor = Color.parseColor("#B22222")
+        val red = (greenColor.red + strength * (redColor.red - greenColor.red)).toInt()
+        val green = (greenColor.green + strength * (redColor.green - greenColor.green)).toInt()
+        val blue = (greenColor.blue + strength * (redColor.blue - greenColor.blue)).toInt()
+        val hexRed = Integer.toHexString(red)
+        val hexGreen = Integer.toHexString(green)
+        val hexBlue = Integer.toHexString(blue)
+        Log.d("RED", hexRed.toString())
+        Log.d("GREEN", hexGreen.toString())
+        Log.d("BLUE", hexBlue.toString())
+        Log.d("COLOR", "#$hexRed$hexGreen$hexBlue")
+        return "#$hexRed$hexGreen$hexBlue"
+
     }
 
     companion object {
